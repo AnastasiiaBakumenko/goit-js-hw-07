22a7ec481f9194bfc6e3ae00c08022b5bce63f44
AnastasiiaBakumenko <bakumenkoanastasia2@gmail.com>
2023-01-06 13:30:57 +0100

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const imgContainer = document.querySelector(".gallery");
+
+const imgMarkup = createNewElementImg (galleryItems);
+
+imgContainer.insertAdjacentHTML('beforeend', imgMarkup);
+
+function createNewElementImg (items){
+    const galleryMarkup = items.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    
+    return galleryMarkup.join('');
+}
+
+    function simpleLiteBox(){
+        let lightBox = new SimpleLightbox('.gallery .gallery__item', {
+            captionsData : 'alt',
+            captionDelay : 250,
+        });
+    }
+    simpleLiteBox();
+
+
+
